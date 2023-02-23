c987ee267df17c33f4ad2ff2a671be29bc6695c3
WTired <125970030+WTired@users.noreply.github.com>
2023-02-23 17:31:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 bot = Bot(token='6285295766:AAFyHSpyhw-PwKDa7jig8lwNQ58utfohejE')
 dp = Dispatcher(bot)
 
+executor.start_polling(dp)
+
 '#Main Menu'
 button1 = KeyboardButton('各科学习群')
 button2 = KeyboardButton('必考科')
@@ -140,5 +142,3 @@ async def kb_answer(message: types.Message):
         await message.answer('请选择：', reply_markup=keyboard13)
     else:
         await message.answer('还未更新，请尽请期待')
-
-
